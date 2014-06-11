da1024778111bfc892e1be83ef6f7555253e4136
uhmin <uhmin@gen-info.osaka-u.ac.jp>
2014-06-11 19:18:25 +0900

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,17 @@
-gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o orf.091208.exe orf.091208.c
-gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o translate.exe translate.091221.c
-gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o revcmp.exe revcmp2.c
-gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o docono.exe docono3.c
-gcc -Wall -lm -O -D_FILE_OFFSET_BITS=64 -o filterN.exe filterN_090601.c
+mkdir -p bin
+echo "compiling orf.exe"
+gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o bin/orf.exe orf.091208.c
 
+echo "compiling translate.exe"
+gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o bin/translate.exe translate.091221.c
+
+echo "compiling revcmp.exe"
+gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o bin/revcmp.exe revcmp2.c
+
+echo "compiling docono.exe"
+gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o bin/docono.exe docono3.c
+
+echo "compiling filterN.exe"
+gcc -Wall -lm -O -D_FILE_OFFSET_BITS=64 -o bin/filterN.exe filterN_090601.c
+
+echo "all compile done!"
