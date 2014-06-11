b8ba6a357a34065ddf3f6b72fecc607cf3940275
uhmin <uhmin@gen-info.osaka-u.ac.jp>
2014-06-11 18:56:37 +0900

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,3 @@
-gcc -Wall -O5 -D_FILE_OFFSET_BITS=64 -o orf.091208.exe orf.091208.c
+gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o orf.091208.exe orf.091208.c
+gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o translate.exe translate.091221.c
+gcc -Wall -O3 -D_FILE_OFFSET_BITS=64 -o revcmp.exe revcmp2.c
